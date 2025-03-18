0eb4e9e610e0d8a3ab408b34df649b3be54f5e30
Matheus Pegorari <pegorari42@gmail.com>
2025-03-17 21:31:56 -0300

    feat: created control of dirty fields and keep the originalVO in the POJO

--- src/dev/pegorari/jrepository/abstractions/AbstractSankhyaEntity.java
+++ src/dev/pegorari/jrepository/abstractions/AbstractSankhyaEntity.java
@@ -1,7 +1,32 @@
 package dev.pegorari.jrepository.abstractions;
+import br.com.sankhya.jape.vo.DynamicVO;
 import dev.pegorari.jrepository.interfaces.SankhyaEntity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public abstract class AbstractSankhyaEntity<T extends SankhyaEntity<T>> implements SankhyaEntity<T> {
+    private transient DynamicVO originalVO;
+    private final transient Map<String, Object> dirtyFields = new HashMap<>();
 
+    @Override
+    public DynamicVO getOriginalVO() {
+        return originalVO;
+    }
+
+    protected void setOriginalVO(DynamicVO vo) {
+        this.originalVO = vo;
+        this.dirtyFields.clear();
+    }
+
+    @Override
+    public void markDirty(String fieldName, Object value) {
+        dirtyFields.put(fieldName, value);
+    }
+
+    @Override
+    public Map<String, Object> getDirtyFields() {
+        return dirtyFields;
+    }
 }
--- src/dev/pegorari/jrepository/interfaces/SankhyaEntity.java
+++ src/dev/pegorari/jrepository/interfaces/SankhyaEntity.java
@@ -2,8 +2,13 @@ package dev.pegorari.jrepository.interfaces;
 
 import br.com.sankhya.jape.vo.DynamicVO;
 
+import java.util.Map;
+
 public interface SankhyaEntity<T extends SankhyaEntity<T>> {
     String getTableName();
     String getEntityName();
     T fromVO(DynamicVO vo);
+    DynamicVO getOriginalVO();
+    void markDirty(String fieldName, Object value);
+    Map<String,Object> getDirtyFields();
 }
